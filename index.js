5d57293836955ac5a7116b35d3e230ddc7ee1706
Lee Graham <lbgraham@mac.com>
2019-07-17 15:01:46 -0700

    Initial implementation of reviews-proxy

--- index.js
+++ index.js
@@ -1,30 +1,18 @@
 const express = require('express');
 const path = require('path');
 
-// const { getBusinessReviews } = require('../database/reviews');
-
 const app = express();
 const port = 3000;
 
-// app.use(express.static(path.join(__dirname, '../public')));
+const proxy = require('express-http-proxy');
 
-// app.param('busId', (req, res, next, business) => {
-//   req.business = getBusinessReviews(business, (err, business) => {
-//     if (err) return next(err);
-//   })
-//     .then((business) => {
-//       req.business = business;
-//       next();
-//     });
-// });
-
-// app.get('/reviews/:busId', ((req, res, next) => {
-//   res.send(req.business);
-// }));
+app.use(express.static(path.join(__dirname, './public')));
+app.use('/reviews', proxy('127.0.0.1:3003'));
+app.use('/photos', proxy('127.0.0.1:3001'));
 
 app.get('/', (req, res) => {
   res.end();
-});
+})
 
 app.listen(port, () => {
   console.log(`Reviews server is running on port ${port}`);
